bde1f4182d64ff703affd9294e4f08189c1df564
HanzlaJavaid <hanzla403@gmail.com>
2021-05-30 05:07:42 +0500

    Email authentication added

--- app/src/main/java/com/example/project_mc/MainActivity.java
+++ app/src/main/java/com/example/project_mc/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.project_mc;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int AUTHUI_REQUEST_CODE = 7192;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.loginRegister);
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        handleLoginRegister();
+                    }
+                });
+    }
+
+    public void handleLoginRegister(){
+
+        List<AuthUI.IdpConfig> provider = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        Intent intent = AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(provider).setTosAndPrivacyPolicyUrls("https://google.com","https://google.com").setIsSmartLockEnabled(false).build();
+        startActivityForResult(intent,AUTHUI_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == AUTHUI_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                ///We have signed in or we have a new user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Log.d("","Msg: "+user.getEmail());
+                if(user.getMetadata().getCreationTimestamp() == user.getMetadata().getLastSignInTimestamp()){
+                    Toast.makeText(this,"Welcome new user",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(this,"Welcome back user",Toast.LENGTH_SHORT).show();
+                }
+                Intent intent = new Intent(this,ProfileActivity.class);
+                startActivity(intent);
+                this.finish();
+            }
+            else{
+                ///Signing failed
+                IdpResponse response = IdpResponse.fromResultIntent(data);
+                if(response == null){
+                    Log.d("","Msg:user has cancelled sign in");
+                }
+                else{
+                    Log.d("","Msg"+ response.getError());
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/project_mc/ProfileActivity.java
@@ -0,0 +1,71 @@
+package com.example.project_mc;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+public class ProfileActivity extends AppCompatActivity {
+    FirebaseUser user;
+    FirebaseAuth firebaseAuth;
+    TextView nameView;
+    ImageView profilePic;
+    Button logoutButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleSignInOptions gso;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+        super.onCreate(savedInstanceState);
+        final Intent logOutintent = new Intent(this,MainActivity.class);
+        setContentView(R.layout.activity_profile);
+        nameView = findViewById(R.id.nameView);
+        profilePic = findViewById(R.id.profilePic);
+        logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        signOut();
+                        startActivity(logOutintent);
+                        finish();
+                    }
+                }
+        );
+        nameView.setText(user.getDisplayName());
+        Picasso.get().load(user.getPhotoUrl()).into(profilePic);
+    }
+    private void signOut() {
+        // Firebase sign out
+        firebaseAuth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        //todo
+                    }
+                });
+    }
+}
